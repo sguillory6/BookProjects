8fccd6001e6fe0ec700d9220e9ae24a24324f6c1
Stanford Guillory <stanford.guillory@gmail.com>
2010-12-04 12:12:51 -0600

    StanG: Initial two examples from book

---
+++ TheRSpecBook/hello/spec/greeter_spec.rb
@@ -0,0 +1,16 @@
+require "rspec"
+
+class RSpecGreeter
+    def greet
+        "Hello RSpec!"
+    end
+end
+
+describe "RSpec Greeter" do
+
+    it "should say 'Hello RSpec!' when it receives the greet() message" do
+        greeter  = RSpecGreeter.new
+        greeting = greeter.greet
+        greeting.should == "Hello RSpec!"
+    end
+end
