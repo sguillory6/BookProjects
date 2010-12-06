86abbbff5edf576d53d97d50cef43a829ca9418f
Stanford Guillory <stanford.guillory@gmail.com>
2010-12-06 16:57:13 -0600

    StanG: Complete refactoring of game.

--- TheRSpecBook/code-breaker/features/codebreaker_starts_game.feature
+++ TheRSpecBook/code-breaker/features/codebreaker_starts_game.feature
@@ -6,6 +6,6 @@ Feature: code-breaker starts game
   Scenario: start game
     Given I am not yet playing
     When I start a new game
-    Then I should see "Welcome to Codebreaker!"
+    Then I should see "Welcome to CodeBreaker!"
     And I should see "Enter guess:"
   
---
+++ TheRSpecBook/code-breaker/lib/codebreaker/game.rb
@@ -0,0 +1,24 @@
+module CodeBreaker
+    class Game
+
+        def initialize(output)
+            @output = output
+        end
+
+        def start(secret)
+            @secret = secret
+            @output.puts 'Welcome to CodeBreaker!'
+            @output.puts 'Enter guess:'
+        end
+
+        def guess(guess)
+            marker = Marker.new(@secret, guess)
+            @output.puts '+' * marker.exact_match_count() +
+                             '-' * marker.number_match_count()
+        end
+
+
+    end
+
+
+end
