357e7437ed249c2f3071c31285a01b954cb07dbc
Stanford Guillory <stanford.guillory@gmail.com>
2010-12-06 18:27:19 -0600

    StanG: Initial creation of depot application

---
+++ AgileWebDevelopmentWithRails/depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '5cb7a6a9fdfd2194c5b20d91fa258fc3f7a64f2dcf5abface79d61a0d7f97ef37c817f8700e6294da4c7f7985aadad21523e3af278c5713f5c73c5e3f4376032'
